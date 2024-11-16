17a954ca264cc603d4414f18d6efb5354ae1d18f
Eric Clemens <clemens72@gmail.com>
2024-11-15 23:04:27 -0500

    implemented the navigation-menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { Menu } from "./components/menu"
 import { Metadata } from "next"
+import Link from 'next/link'
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
 import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
 
@@ -23,7 +24,19 @@ export const metadata: Metadata = {
 export default function Web() {
   return (
     <>
-	  <Menu />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+          <NavigationMenuContent>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Documentation
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
@@ -31,10 +44,12 @@ export default function Web() {
               opporTune
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-               The right brain discovers the profound: taking the road less traveled, and sharing the tale in full color. Unfortunately this lack of conformity hinders organizing, planning and communicating among other things. opporTune aims to provide tools and resources for those less structured.
+              The right brain discovers the profound: taking the road less traveled, and sharing the tale in full color.
+              Unfortunately this lack of conformity hinders organizing, planning and communicating among other things.
+              opporTune aims to provide tools and resources for those less structured.
             </p>
             <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
-             Log In
+              Log In
             </Button>
             <Button
               href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
@@ -50,7 +65,7 @@ export default function Web() {
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
             {LP_GRID_ITEMS.map((singleItem) => (
               <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
+                <div className="mb-4 flex size-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:size-12">
                   {singleItem.icon}
                 </div>
                 <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
--- lp-items.tsx
+++ lp-items.tsx
@@ -1,7 +1,47 @@
 export const LP_GRID_ITEMS = [
   {
-    title: "Next.js",
-    description: "Fast by default, with config optimized for performance.",
+    title: "Scheduling",
+    description: "Maintain band calendars all at once and keep up with bandmate availability.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Sketch Log",
+    description: "Share songs at any point of completion with lyrics, audio and comments.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Knowledge Base",
+    description: "Your reference guide for any questions you can come up with.",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +59,7 @@ export const LP_GRID_ITEMS = [
       </svg>
     ),
   },
-  {
+/*   {
     title: "Tailwind CSS",
     description: "A utility-first CSS framework for rapid UI development.",
     icon: (
@@ -54,10 +94,10 @@ export const LP_GRID_ITEMS = [
         <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
       </svg>
     ),
-  },
+  }, */
   {
-    title: "Extremely strict TypeScript",
-    description: "With `ts-reset` library for ultimate type safety.",
+    title: "Privacy and Sharing Settings",
+    description: "Upload all your files to one place with full control over who can access what.",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -75,9 +115,9 @@ export const LP_GRID_ITEMS = [
       </svg>
     ),
   },
-  {
-    title: "Bundle analyzer plugin",
-    description: "Keep an eye on your bundle size.",
+/*   {
+    title: "Stats and Review",
+    description: "Track all your progress and lean into your strengths!",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -135,47 +175,8 @@ export const LP_GRID_ITEMS = [
       </svg>
     ),
   },
-  {
-    title: "Storybook",
-    description: "Create, test, and showcase your components.",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-6 w-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
-        />
-      </svg>
-    ),
-  },
-  {
-    title: "Smoke Testing & Acceptance Tests",
-    description: "For confidence in your deployments.",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-6 w-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
-        />
-      </svg>
-    ),
-  },
-  {
+
+   {
     title: "Conventional commits git hook",
     description: "Keep your commit history neat and tidy.",
     icon: (
@@ -412,5 +413,5 @@ export const LP_GRID_ITEMS = [
         />
       </svg>
     ),
-  },
+  }, */
 ]
